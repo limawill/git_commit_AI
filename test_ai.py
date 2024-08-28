faba55873a38b27801575944293b242a316cca23
limawill <limawill83@gmail.com>
2024-08-27 21:23:36 -0300

    O código foi atualizado para incluir uma chave de API e um teste para a função . A função agora executa um comando git e retorna a saída, além de tratar erros. A atualização inclui também a definição de uma variável  que irá servir como input para o modelo de linguagem.

--- test_ai.py
+++ test_ai.py
@@ -1,9 +1,31 @@
 import os
+import subprocess
 import google.generativeai as genai
 
 
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
+genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 model = genai.GenerativeModel('gemini-1.5-flash')
-response = model.generate_content("Qual o coletivo de cachorro?")
+
+
+def run_git_command(command):
+    try:
+        # Executa o comando git e captura a saída
+        result = subprocess.run(
+            command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao executar o comando: {e}")
+        print(f"Mensagem de erro: {e.stderr}")
+        return None
+
+
+output = run_git_command(["git", "diff"])
+
+Teste = f"Analisar o retorno do comando git diff e criar uma frase de no maximo de 3 linhas focando na alterações do código para serem utilizadas no comando git commit: {
+    output} "
+
+
+response = model.generate_content(str(Teste))
 print(response.text)
+
